ef90a7f489b0cd15e4b423fdba0b695c4faa6f96
Bohdan Datsko <bohdandatsko5@gmail.com>
2023-10-22 23:13:45 +0300

    KEY HIDE

--- teamsite/teamsite/settings.py
+++ teamsite/teamsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sp*x_l1hm(u%*d6#+83y@!9j$!(l*a+v=_+emc0u5^d0(9+a1b'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
